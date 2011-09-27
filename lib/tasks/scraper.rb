b364c7d953032637e59b5697f54d1d45c3e92041
Jonathan Smith <jonathan@impacti.org>
2011-09-27 09:16:32 +0100

    parses dates, and started to figure out which team is home

--- lib/tasks/scraper.rb
+++ lib/tasks/scraper.rb
@@ -12,20 +12,25 @@
 # 
 
 require 'open-uri'
-require 'nokogiri'
+require 'nokogiri'          
+require 'date'
 
-i = 0
-until i == 15
+15.times do |i|
   html = "http://elsworth.play-cricket.com/scoreboard/results.asp?page=#{i.to_s}&startDay=&quickSearch=&startYear=&team=&seasonID=&fromForm=1&endMonth=&startMonth=&endYear=&endDay=&type="
-  puts html
+  # puts html
   doc = Nokogiri::HTML(open(html))
   t = doc.xpath('//table[@class = "data"]/tr')
+  t.shift
   t.each do |y|
-    d = y.children[0].text      #Sat 4th Aug 2007
-    print d.to_s
-    puts y.children[5].text unless y.children[5].text.strip! == "Cancelled"
+    data = y.children
+    match_date = Date.parse(data[0].text)
+    home = true if data[1].text.match(/href/) 
+    puts "Home -> #{home} : #{data[1]} : #{data[3].text} : #{data[5].text}"
+    
+    # puts data[5].text unless data[5].text.strip! == "Cancelled"
+
+    #TODO Match.new(:home_team_id => "", :away_team_id => , :match_date => )
   end
-  i += 1
 end
 
 
