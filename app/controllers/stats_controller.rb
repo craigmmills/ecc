790cc7a67c0a6b077fdf3a3f8211455c3691860f
Jonathan Smith <jonathan@impacti.org>
2011-09-28 18:55:18 +0100

    first crack at guessing results - will work for now, but might not be accurate

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,8 +4,17 @@ class StatsController < ApplicationController
     @all_results = Matches.all
   end                         
   
-  def guess_result(home_runs, home_wickets, away_runs, away_wickets)
-    
-    
+  def guess_result(home_runs, away_runs)
+    # TODO: For simplicity now, all results are from the home team point of view - this won't work in multi-team environment
+    # TODO: Need to check the veracity of this - it looks too simple
+    case home_runs <=> away_runs
+      when 1
+        return "W" # W for Win!
+      when 0
+        return "D" # D for draw
+      when -1
+        return "L" # L for loss
+      end
   end
+    
 end
