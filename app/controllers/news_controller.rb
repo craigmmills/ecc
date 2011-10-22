a9b3762b1e0144850bc96db04ee612c10c61530f
Craig Mills <craig.mills@unep-wcmc.org>
2011-10-22 14:44:20 +0100

    added editing page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -46,6 +46,19 @@ class NewsController < ApplicationController
    end
   end
   
+  def edit
+    @news = News.find(params[:id])
+  end
+  
+  def destroy
+    @news = News.find(params[:id])
+    @news.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { head :ok }
+    end
+  end
   
 
 end
