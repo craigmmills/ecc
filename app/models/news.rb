e7fd6077c03d5c9f27c6442292af848c4524d79c
Craig Mills <craig.mills@unep-wcmc.org>
2012-02-15 08:33:16 +0000

    fix paperclip stuff

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,7 +1,5 @@
 class News < ActiveRecord::Base
   has_attached_file :pics, :styles => { :main => "940x525>", :thumb => "100x100>" },
-                    :storage => :Dropboxstorage,
-                    :path => "/:attachment/:attachment/:id/:style/:filename"
   #scope :latest_news, limit(10).news.order("news.created_at DESC")
   scope :latest_news, order("news.created_at DESC")
 end
